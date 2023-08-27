06c1def5d65efee78786603bf200f908da51b672
PullZZZ <ds@dan415.ru>
2023-08-28 01:43:28 +0700

    add custom pagination

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -136,8 +136,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
     ),
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 5,
+    'DEFAULT_PAGINATION_CLASS': 'api.pagination.PageLimitPagination',
 }
 
 DJOSER = {
